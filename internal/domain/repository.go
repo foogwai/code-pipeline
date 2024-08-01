20e59afc824d947f57442230723c428ed4c65136
Ethan Huang <ehuang@exome.co>
2024-07-31 20:14:27 -0700

    Clear`

--- internal/domain/repository.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-// Repository defines the methods required for a data repository that stores POST data.
-type Repository interface {
-	SavePostData(postData PostData) error
-}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/crseat/example-data-pipeline/internal/infrastructure"
-)
-
-func main() {
-	infrastructure.StartServer()
-}
